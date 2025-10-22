2965a3b31850f9a9780868994398cbec52d5ffa7
Viorela-Bloj <bviorelaanamaria@yahoo.com>
2025-10-22 23:07:48 +0200

    .
    .

--- ProyectoInicalMPOT1.1/src/Main.java
+++ ProyectoInicalMPOT1.1/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("Proyecto metodos");
+        System.out.println("Vamos a realizar llamadas a metodos");
+        saludar("Vio");
+        saludar("Borja");
+        saludar("Ana");
+        despedir();
+        //Operaciones.sumar(4,4); esta es una operación peligros, poniendo el static en la carpera de operaciones donde la suma
+        Operaciones opercionesMatematicas = new Operaciones();
+        opercionesMatematicas.sumar(1,4);
+        opercionesMatematicas.resta(8,2);
+        opercionesMatematicas.resta(8,5);
+        opercionesMatematicas.sumar(10,50);
 
     }
+
+        public static void saludar(String nombre){
+            System.out.println("Hola " +nombre);
+            System.out.println("Encantado de saludarte, vamos a practicar");
+            System.out.println("Vamos a practicar el llamado de metodos");
+            System.out.println("Animo, mo decaigas en tu tarea");
+
+        }
+
+        public static void despedir(){
+        System.out.println("A cerrar el portatil, a pasar la tarde a casa");
+        }
+
+
+
+
+
 }
---
+++ ProyectoInicalMPOT1.1/src/Operaciones.java
@@ -0,0 +1,11 @@
+public class Operaciones {
+    //Tiene variables, y metodos que puedes ejecutar.
+    public  void sumar(int op1, int op2){
+        int resultado = op1+op2;
+        System.out.println("El resultado de la suma es " +resultado);
+    }
+    public void resta(int op1, int op2){
+        int resultado =op1-op2;
+        System.out.println("El resiltado de la suma es "+ resultado);
+    }
+}
